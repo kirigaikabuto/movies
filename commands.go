2071e0aebdc0a123f8ecf4a4f09ec62d30872605
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-11-22 14:16:51 +0600

    sdsddsd

--- commands.go
+++ commands.go
@@ -1,1 +1,22 @@
 package movies
+
+type AddMovieCommand struct {
+	Name   string `json:"name"`
+	Rating int64  `json:"rating"`
+}
+
+type UpdateMovieCommand struct {
+	Name   *string `json:"name"`
+	Rating *int64  `json:"rating"`
+}
+
+type DeleteMovieCommand struct {
+	Id int64 `json:"id"`
+}
+
+type GetMovieCommand struct {
+	Id int64 `json:"id"`
+}
+
+type ListMovieCommand struct {
+}
